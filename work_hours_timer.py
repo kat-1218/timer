d9875f7c7e16a85aa3e04b70b53935a51ca7f1c4
kat-1218 <81578397+kat-1218@users.noreply.github.com>
2021-04-08 17:14:59 +0100

    Work hours timer
    V2 or work hours timer, fixed error with break time subtraction

--- work_hours_timer.py
+++ work_hours_timer.py
@@ -28,16 +28,16 @@ class TimeCalculator:
         else:
             print("Either 'y' or 'n' should be entered")
             self.initiate_timer('yes')
-        return self.start_time, value
+        return self.start_time
 
     def time_log_questions(self, value):
         answer = input('Log another time? (y/n)')
         if answer == 'y':
             value_1 = input('Are you taking a break? (y/n)')
             if value_1 == 'y':
-                self.log_break_time(value_1)
+                self.log_break_time()
             elif value_1 == 'n':
-                self.log_decimal_hours(value_1)
+                self.log_decimal_hours()
             else:
                 print("Either 'y' or 'n' should be entered")
                 self.time_log_questions('y')
@@ -47,14 +47,14 @@ class TimeCalculator:
         else:
             print("Either 'y' or 'n' should be entered")
             self.time_log_questions('y')
-        return value_1, self.start_break, self.stop_break, self.break_time
+        return self.start_break, self.stop_break, self.break_time
     
-    def log_decimal_hours(self, value_1):
+    def log_decimal_hours(self):
         value_3 = input('Are you stopping for the day? (y/n)')
         if value_3 == 'y':
-            self.stop_time_calc(value_3)
+            self.stop_time_calc()
             print("Calculating decimal hours")
-            self.calculate_hours(value_3)
+            self.calculate_hours()
         elif value_3 == 'n':
             print("Okay, waiting for next time log")
             self.time_log_questions('y')
@@ -63,13 +63,13 @@ class TimeCalculator:
             self.time_log_questions('y')
         return value_3
 
-    def log_break_time(self, value_1):
-        self.start_break_calc(value_1)
+    def log_break_time(self):
+        self.start_break_calc()
         print("break time started")
         value_2 = input('Have you completed your break? (y/n)')
         if value_2 == 'y':
-            self.stop_break_calc(value_2)
-            self.break_time_calc(value_2)
+            self.stop_break_calc()
+            self.break_time_calc()
             print("break time logged")
             self.time_log_questions('y')
         elif value_2 == 'n':
@@ -80,31 +80,31 @@ class TimeCalculator:
             self.time_log_questions('y')
         return self.break_time
 
-    def stop_time_calc(self, value_3):
+    def stop_time_calc(self):
         self.stop_time = datetime.datetime.now()
         print(self.stop_time)
         print("Stop time logged")
         return self.stop_time
 
 
-    def start_break_calc(self, value_1):
+    def start_break_calc(self):
         self.start_break = datetime.datetime.now()
         print(self.start_break)
         return self.start_break
 
-    def stop_break_calc(self, value_2):
+    def stop_break_calc(self):
         self.stop_break = datetime.datetime.now()
         print(self.stop_break)
         return self.stop_break
 
-    def break_time_calc(self, value_2):
+    def break_time_calc(self):
         self.break_time = self.stop_break - self.start_break
         return self.break_time
 
-    def calculate_hours(self, value_3):
-        if hasattr('TimeCalculator', 'break_time') == True:
+    def calculate_hours(self):
+        if hasattr(self, 'break_time') == True:
             raw_hours = (self.stop_time - self.start_time) - self.break_time
-        elif hasattr('TimeCalculator', 'break_time') == False:
+        elif hasattr(self, 'break_time') == False:
             raw_hours = self.stop_time - self.start_time
         secs = raw_hours.total_seconds()
         mins = int(secs/60) % 60
